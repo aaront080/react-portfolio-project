248a4bddd23394a5da0663897c0aa97300b0c73a
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-12-04 15:42:08 -0800

    app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,57 @@
-import logo from './logo.svg';
+import "./App.css";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+
+function App() {
+    const [currentTab, setCurrentTab] = useState("about");
+    const renderTab = () => {
+        switch (currentTab) {
+            case "about":
+              return <About />;
+            case "portfolio":
+              return <Portfolio />;
+            case "contact":
+              return <Contact />;
+            case "resume":
+              return <Resume />;
+            default:
+              return null;        
+        }
+    };
+
+    return (
+      <div>
+          <div className="mobile-header">
+              <Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+          </div>
+          <div>
+              <main>{renderTab()}</main>
+          </div>
+          <div>
+            <Footer></Footer>
+          </div>
+      </div>
+    );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+/* import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +75,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
