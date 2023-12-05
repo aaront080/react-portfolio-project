2dfe44c7ae5ff9f75ff68b3bf81e5ba221e3d562
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-12-04 17:01:28 -0800

    header index

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navigation from "../Navigation";
+
+function Header(props) {
+    const { currentTab, setCurrentTab } = props;
+
+    return (
+        <header>
+            <div>
+                <h2>Aaron's Portfolio</h2>
+            </div>
+            <div>
+                <Navigation currentTab={currentTab} setCurrentTab={setCurrentTab}></Navigation>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
